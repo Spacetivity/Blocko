352d4790ab1cdd1c94fa02e7b4e4bce581fa6ead
Tobias Heimboeck <tobiasheimboeck@outlook.com>
2024-03-28 10:06:19 +0100

    Refactored gradle structure

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    java
-    kotlin("jvm") version "1.9.0"
+    kotlin("jvm") version "1.9.23"
 }
 
 group = "net.spacetivity.blocko"
@@ -26,42 +25,20 @@ dependencies {
     compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
     compileOnly("net.spacetivity.inventory:inventory-api:1.0-SNAPSHOT")
 
-    implementation(group = "org.mariadb.jdbc", name = "mariadb-java-client", version = "3.0.7")
+    compileOnly(group = "org.mariadb.jdbc", name = "mariadb-java-client", version = "3.0.7")
 
-    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
-    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
-    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+    api("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    api("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+    api("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
 
-    implementation(group = "org.slf4j", name = "slf4j-api", version = "1.7.25")
-    implementation(group = "org.slf4j", name = "slf4j-simple", version = "1.7.25")
+    compileOnly(group = "org.slf4j", name = "slf4j-api", version = "1.7.25")
+    compileOnly(group = "org.slf4j", name = "slf4j-simple", version = "1.7.25")
 }
 
-tasks.test {
-    useJUnitPlatform()
+kotlin {
+    jvmToolchain(17)
 }
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "17"
-}
-
-val compileKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-    jvmTarget = "17"
-}
-
-val compileTestKotlin: KotlinCompile by tasks
-compileTestKotlin.kotlinOptions {
-    jvmTarget = "17"
-}
-
-tasks.withType<Jar> {
-    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-    from(sourceSets.main.get().output)
-    dependsOn(configurations.runtimeClasspath)
-    from({ configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) } })
-}
-
-task("sourcesJar", type = Jar::class) {
-    from(sourceSets.main.get().allSource)
-    archiveClassifier.set("sources")
 }
